970223e2b8148f651d872d1b8478ae98b24d929e
ahmharis <83187223+ahmharis@users.noreply.github.com>
2025-10-24 16:46:10 +0800

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css'
+
+// File ini memuat file CSS global untuk seluruh aplikasi Anda
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
